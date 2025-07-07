28e3d87733b5e1bfdc4dbe08b36151abf7a1081e
Dillow7 <randriamirija4@gmail.com>
2025-07-08 01:38:18 +0300

    modif

--- ws/controllers/PaiementPretController.php
+++ ws/controllers/PaiementPretController.php
@@ -48,40 +48,4 @@ class PaiementPretController {
         PaiementPret::delete($id);
         Flight::json(['message' => 'Paiement supprimé']);
     }
-
-    public static function getTotalPaiementsClient($id_client, $debut_mois, $debut_annee, $fin_mois, $fin_annee) {
-        $db = getDB();
-        $query = "
-            SELECT SUM(pp.montant_paye) as total_paye
-            FROM paiements_pret pp
-            JOIN prets p ON pp.id_pret = p.id_pret
-            WHERE p.id_client = ?
-            AND pp.date_paiement >= ?
-            AND pp.date_paiement <= ?
-        ";
-        $debut = "$debut_annee-$debut_mois-01";
-        $fin = "$fin_annee-$fin_mois-" . date('t', strtotime("$fin_annee-$fin_mois-01"));
-        $stmt = $db->prepare($query);
-        $stmt->execute([$id_client, $debut, $fin]);
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        $total = $result['total_paye'] ?? 0;
-        Flight::json(['id_client' => $id_client, 'total_paye' => $total]);
-    }
-
-    public static function getTotalPaiementsPeriode($debut_mois, $debut_annee, $fin_mois, $fin_annee) {
-        $db = getDB();
-        $query = "
-            SELECT SUM(pp.montant_paye) as total_paye
-            FROM paiements_pret pp
-            WHERE pp.date_paiement >= ?
-            AND pp.date_paiement <= ?
-        ";
-        $debut = "$debut_annee-$debut_mois-01";
-        $fin = "$fin_annee-$fin_mois-" . date('t', strtotime("$fin_annee-$fin_mois-01"));
-        $stmt = $db->prepare($query);
-        $stmt->execute([$debut, $fin]);
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        $total = $result['total_paye'] ?? 0;
-        Flight::json(['total_paye' => $total]);
-    }
 }
