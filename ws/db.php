13785a036b0b02a68c1cb983d318a07bab0eb3fc
Dillow7 <randriamirija4@gmail.com>
2025-07-07 14:33:32 +0300

    Update db.php

--- ws/db.php
+++ ws/db.php
@@ -1,15 +1,21 @@
 <?php
 function getDB() {
-    $host = 'localhost';
-    $dbname = 'tp_flight';
-    $username = 'root';
-    $password = '';
+    static $pdo = null;
+    if ($pdo === null) {
+        $host = '127.0.0.1';
+        $dbname = 'tp_flight';
+        $username = 'root';
+        $password = '';
 
-    try {
-        return new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]);
-    } catch (PDOException $e) {
-        die(json_encode(['error' => $e->getMessage()]));
+        try {
+            $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+        } catch (PDOException $e) {
+            die(json_encode(['error' => $e->getMessage()]));
+        }
     }
+    return $pdo;
 }
+
+?>
